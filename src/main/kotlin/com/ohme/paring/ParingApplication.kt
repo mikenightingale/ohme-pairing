3d4e2eb39b50dc688f6dfdb1361a9f9888f1fdb0
Michael Nightingale <michaeln.public@icloud.com>
2024-09-16 12:24:51 +0100

    chore: initial commit

---
+++ src/main/kotlin/com/ohme/paring/ParingApplication.kt
@@ -0,0 +1,11 @@
+package com.ohme.paring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParingApplication>(*args)
+}
